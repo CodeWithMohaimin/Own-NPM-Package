f209c4ad16266bb25a908b87cd6889f261c29770
programmermohaimin <you@example.com>
2022-06-06 16:18:35 +0600

    my own npm package

---
+++ app.js
@@ -0,0 +1,10 @@
+var _ = require('underscore');
+
+var arr = [1, 2, 3, 4, 5];
+
+_.each(arr, function (element) {
+    console.log(element);
+    console.log('hello programmers');
+    console.log('hello world');
+    
+});
